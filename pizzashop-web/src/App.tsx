b3e0d7e1434574afef1f062c66279f78e6328151
Matheus <matspurio@gmail.com>
2024-08-30 09:56:54 -0300

    adicionando lint

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,15 +1,12 @@
-
-import { Button } from './components/ui/button'
-import './global.css'
+import { Button } from './components/ui/button';
+import './global.css';
 
 export function App() {
-
   return (
     <>
-     <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-     </div>
+      <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+      </div>
     </>
-  )
-} 
-
+  );
+}
